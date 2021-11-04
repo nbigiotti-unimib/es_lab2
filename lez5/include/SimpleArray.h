0f67f2da6443774da4451ba480d522dc55a8fecf
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-04 09:48:01 +0100

    exercise 3 done

---
+++ lez5/include/SimpleArray.h
@@ -0,0 +1,31 @@
+template <typename T>
+class SimpleArray {
+public:
+    // Costruttore
+    SimpleArray (const int & elementsNum) { 
+        elementsNum_p = elementsNum;
+        elements_p = new T[elementsNum_p];
+    }
+    // Distruttore
+    ~SimpleArray () {
+        delete elements_p;
+    }
+    T & element (const int& i)  {
+        if(i >= elementsNum_p) {
+            return -1;
+        }
+        return elementsNum_p[i];
+    }
+    // Overloading di operator[]
+    T & operator[] (const int& i) {
+        if(i >= elementsNum_p) {
+            return -1;
+        }
+        return elementsNum_p[i];
+    }
+
+private:
+    
+    int elementsNum_p;
+    T * elements_p;
+};
