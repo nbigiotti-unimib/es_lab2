0779fa84fa63756403d97bc65528f53a514c5666
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-11 12:24:38 +0100

    seventh exercise done

--- lez6/src/es_67/main_67.cpp
+++ lez6/src/es_67/main_67.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <TF1.h>
+#include <TCanvas.h>
+#include <TMarker.h>
+#include <TApplication.h>
+
 
 int main(int argc , char * argv[]) {
-	printf("TEST: OK");
+
+	TApplication myApp ("myAPP" , &argc , argv);
+	TF1 funct1 ("fa1", "x^2+7.3*x+4", -10, 10);
+	TCanvas c1;
+	funct1.Draw ();
+
+	TMarker minimo (-3.65, -9.322, 20);
+	minimo.Draw ();
+	myApp.Run();
 	return 0;
 }
