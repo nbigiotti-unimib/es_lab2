96b28adc0703cfe1c1b343a0a494ab16a7170e35
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-11 10:56:42 +0100

    third exercise done

--- lez6/src/es_63/main_63.cpp
+++ lez6/src/es_63/main_63.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
 
+int fattoriale (int num);
+
 int main(int argc , char * argv[]) {
-	printf("TEST: OK");
+	int N;
+	std::cout << "Inserire il numero di cui si vuole calcolare il fattoriale" << std::endl;
+	std::cin >> N;
+	std::cout << "Il fattoriale vale: " << fattoriale(N) << std::endl;
 	return 0;
+}
+
+int fattoriale (int num) {
+	if(num == 1) {
+		return 1;
+	}
+
+	return (num * fattoriale(num - 1));
 }
