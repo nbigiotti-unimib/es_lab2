6b29aec543e6628e9498d1e1be6e9df801bf4f34
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-11 10:20:30 +0100

    first exercise done

--- lez6/src/es_61/main_61.cpp
+++ lez6/src/es_61/main_61.cpp
@@ -1,6 +1,20 @@
 #include <iostream>
+#include <cmath>
 
 int main(int argc , char * argv[]) {
-	printf("TEST: OK");
+	double interv[3] = {0 , 2 , 4};
+	double min = -1;
+	double precision = 0.00001;
+
+	while (interv[2] - interv[0] > precision) {
+		if (cos(interv[0]) * cos(interv[1]) < 0) {
+			interv[2] = interv[1];
+			interv[1] = (interv[0] + interv[2]) / 2;
+		} else {
+			interv[0] = interv[1];
+			interv[1] = (interv[0] + interv[2]) / 2;
+		}
+	}
+	std::cout << "Il minimo è compreso fra: " << interv[0] << " e " << interv[2] << std::endl;
 	return 0;
 }
