81b9a25a4875df5efb5d7d35b168844a0a059ba2
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-11 10:47:14 +0100

    second exercise done

--- lez6/src/es_62/main_62.cpp
+++ lez6/src/es_62/main_62.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <cmath>
+
+int zeroes(double inf , double sup);
 
 int main(int argc , char * argv[]) {
-	printf("TEST: OK");
+	zeroes(0,4);
+	return 0;
+}
+
+int zeroes(double inf , double sup) {
+	double precision = 0.001;
+	if ((sup - inf) < precision) {
+		std::cout << "Il minimo è compreso fra: " << inf << " e " << sup << std::endl;
+		return 0;
+	}
+	if (cos(inf) * cos((inf + sup) / 2.0) < 0) {
+		sup = (inf + sup) / 2.0;
+	} else {
+		inf = (inf + sup) / 2.0;
+	}	
+	zeroes(inf , sup);	
 	return 0;
 }
