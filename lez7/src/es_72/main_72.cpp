c94ff8f8849c7178ea8f4f76b75f7b5cb574bf73
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-18 11:00:40 +0100

    second exercise done

--- lez7/src/es_72/main_72.cpp
+++ lez7/src/es_72/main_72.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "../../include/exp.h"
+#include <TH1F.h>
+#include <TCanvas.h>
 
 int main(int argc , char * argv[]) {
-	
+	double t_0  = 1.0, t_M = 10.0;
+	int N = 1000000;
+
+	exp_pdf pdf1 (t_0);
+
+	TH1F ist ("ist" , "Esponenziale" , 75 , 0 , 30);
+	double t_int = 0;
+	int count = 0;
+	for(int i = 0 ; i < N ; ++i) {
+		t_int = 0;
+		count = 0;
+		while (t_int < t_M) {
+			t_int += pdf1.generate_exp();
+			++count;
+		}
+		ist.Fill(count);		
+	}
+
+	TCanvas c1 ;
+	ist.Draw () ;
+	c1.Print ("misc_output/es_72.png", "png") ;
+
 	return 0;
 }
