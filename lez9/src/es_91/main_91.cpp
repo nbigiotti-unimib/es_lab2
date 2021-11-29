93a370a7d5c02952cf972785e2256bf31aec9caf
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-29 09:27:00 +0100

    first exercise done

--- lez9/src/es_91/main_91.cpp
+++ lez9/src/es_91/main_91.cpp
@@ -1,6 +1,22 @@
 #include <iostream>
+#include <vector>
+#include <libLAB2/pdf.h>
 
 int main(int argc , char * argv[]) {
-	printf("TEST: OK");
+	
+	if (argc < 3) {
+		printf("Usage: %s <t_0> <n_eventi>\n" , argv[0]);
+		return 1;
+	}
+	
+	double t_0 = atoi(argv[2]);
+	int n_eventi = atoi(argv[2]);
+	std::vector<double> tempi;
+	pdf exp;
+
+	for (int i = 0 ; i  < n_eventi ; ++i) {
+		tempi.push_back(exp.generate_exp(t_0));
+	}
+
 	return 0;
 }
