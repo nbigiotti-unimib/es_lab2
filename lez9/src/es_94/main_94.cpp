44b1ad8d91bc7b0d969fefa1eadfcf84a39f7e29
Nicolas Bigiotti <nicobigio01@dell-laptop>
2021-11-29 11:36:54 +0100

    fourth exercise done

--- lez9/src/es_94/main_94.cpp
+++ lez9/src/es_94/main_94.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <cmath>
+
+#include <TF1.h>
+#include <TCanvas.h>
+#include <libLAB2/pdf.h>
+
+Double_t loglikelihood (Double_t * x, Double_t * par);
 
 int main(int argc , char * argv[]) {
-	printf("TEST: OK");
+	
+	TF1 f_likelihood ("f_likelihood" , loglikelihood , 1 , 1000 , 1);
+	f_likelihood.SetParameter(0 , 100);
+
+	TCanvas c1;
+	f_likelihood.Draw();
+	c1.Print("misc_output/es_94_2.png" , "png");
+
 	return 0;
+}
+
+Double_t loglikelihood (Double_t * x, Double_t * par) {
+  	double likelihood = 0, n_eventi = par[0];
+	
+	pdf esp;
+
+	for (int i = 0 ; i  < n_eventi ; ++i) {
+		double temp = esp.generate_exp(x[0]);
+		likelihood += log(esp.generate_exp(temp));
+		printf("DEBUG: %lf ; likelihood: %lf\n" , temp , likelihood);
+	}
+	if (likelihood <= 0)
+		return 1.;
+
+	return likelihood;
 }
