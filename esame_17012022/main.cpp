97381df7c5e97ec507a6111a85f342d781e494fb
Nicolas Bigiotti <nicobigio01@gmail.com>
2022-01-17 12:09:32 +0100

    All ok

--- esame_17012022/main.cpp
+++ esame_17012022/main.cpp
@@ -15,11 +15,13 @@ int main () {
 
     //Parte 1 - Generazione punti secondo pdf triangolare
     int n_events = 1000;
+    double point_max {-100.} , point_min {100.};
     vector<double> points;
 
     //Generiamo <n_events> punti distribuiti secondo una pdf triangolare
     for(int i = 0 ; i < n_events ; ++i) {
-        points.push_back(triangle_tac(0. , 3.));
+        double tmp_point {triangle_tac(0. , 3.)};
+        points.push_back(tmp_point);
     }
 
     //Riempiamo L'istogramma con i punti generati
@@ -38,6 +40,8 @@ int main () {
 
     //Parte 2 - Toy experiments
     int n_toy {100000} , toy_events {100};
+    double mean_max{-100.} , mean_min{100.};
+    double std_dev_max{-100.} , std_dev_min{100.};
     vector<double> toy_mean , toy_std_dev;
 
     points.clear();    
@@ -45,30 +49,38 @@ int main () {
     //Eseguiamo un numero <n_toy> di toy experiments,
     //conservando i valori di media e varianza associati
     //ad ogni campione
+    
     for (int i {0} ; i < n_toy ; ++i) {
         for(int k {0} ; k < toy_events ; ++k) {
             points.push_back(triangle_tac(0. , 3.));
         }
-        toy_mean.push_back(mean(points));
-        toy_std_dev.push_back(std_dev_bessel(points , toy_mean.back()));
+        
+        double tmp_mean {mean(points)};
+        double tmp_std_dev {std_dev_bessel(points , tmp_mean)};
+        toy_mean.push_back(tmp_mean);
+        toy_std_dev.push_back(tmp_std_dev);
+        if (tmp_mean > mean_max)
+            mean_max = tmp_mean;
+        if (tmp_mean < mean_min)
+            mean_min = tmp_mean;
+        if (tmp_std_dev > std_dev_max)
+            mean_max = tmp_std_dev;
+        if (tmp_std_dev < std_dev_min)
+            std_dev_min = tmp_std_dev;
         points.clear();
     }
     
     //Creiamo e stampiamo gli istogrammi relativi ai risultati dei toy experiments
-    TH1F mean_hist ("mean_hist" , "Istogramma" , 50 , 1.8 , 2.4);
-    for (vector<double>::iterator it = toy_mean.begin() ; it != toy_mean.end() ; ++it) {
-        mean_hist.Fill(*it);
+    TH1F mean_hist ("mean_hist" , "Istogramma" , 50 , mean_min , mean_max);
+    TH1F std_dev_hist ("std_dev_hist" , "Istogramma" , 50 , std_dev_min , std_dev_max);
+    for (int i = 0 ; i < n_toy ; ++i) {
+        mean_hist.Fill(toy_mean[i]);
+        std_dev_hist.Fill(toy_std_dev[i]);
     }
 
     TCanvas c2;
     mean_hist.Draw();
     c2.Print("mean.png" , "png"); 
-
-
-    TH1F std_dev_hist ("std_dev_hist" , "Istogramma" , 50 , 0.54 , 0.86);
-    for (vector<double>::iterator it = toy_std_dev.begin() ; it != toy_std_dev.end() ; ++it) {
-        std_dev_hist.Fill(*it);
-    }
 
     TCanvas c3;
     std_dev_hist.Draw();
