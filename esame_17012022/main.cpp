77b385f91102c345266c4bda31058d6334329990
Nicolas Bigiotti <nicobigio01@gmail.com>
2022-01-17 10:08:26 +0100

    Exam task 5 ready for test

--- esame_17012022/main.cpp
+++ esame_17012022/main.cpp
@@ -31,5 +31,39 @@ int main () {
     hist.Draw();
     c1.Print("plot.png");
 
+    int n_toy {1000} , toy_events {100};
+    vector<double> toy_mean , toy_std_dev;
+
+    points.clear();    
+
+    for (int i {0} ; i < n_toy ; ++i) {
+        for(int k {0} ; i < toy_events ; ++k) {
+            points.push_back(triangle_tac(0. , 3.));
+        }
+        toy_mean.push_back(mean(points));
+        toy_std_dev.push_back(std_dev_bessel(points , toy_mean.back()));
+        points.clear();
+    }
+
+    TH1F mean_hist ("mean_hist" , "Istogramma" , 50 , 0. , 10.);
+
+    for (vector<double>::iterator it = toy_mean.begin() ; it != toy_mean.end() ; ++it) {
+        mean_hist.Fill(*it);
+    }
+
+    TCanvas c2;
+    mean_hist.Draw();
+    c1.Print("mean.png" , "png"); 
+
+    TH1F std_dev_hist ("mean_hist" , "Istogramma" , 50 , 0. , 3.);
+
+    for (vector<double>::iterator it = toy_std_dev.begin() ; it != toy_std_dev.end() ; ++it) {
+        std_dev_hist.Fill(*it);
+    }
+
+    TCanvas c3;
+    std_dev_hist.Draw();
+    c1.Print("std.png" , "png");
+
     return 0;
 }
